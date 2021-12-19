fc4f56a2b4b58262e3979e4c62fea83d66229a07
clemoctohub <59572299+clemoctohub@users.noreply.github.com>
2021-12-19 23:10:18 +0100

    Test finished
    - test added (already done before)
    - shielding back with corresponding answer

--- project/back-end/lib/db.js
+++ project/back-end/lib/db.js
@@ -8,16 +8,32 @@ const db = level(__dirname + '/../db')
 module.exports = {
   channels: {
     create: async (channel) => {
-      if(!channel.name || !channel.users) throw Error('Invalid channel')
-      const id = uuid()
-      await db.put(`channels:${id}`, JSON.stringify(channel))
-      return merge(channel, {id: id})
+      try{
+        if(!channel.name || !channel.users) throw Error('Invalid channel')
+        const id = uuid()
+        await db.put(`channels:${id}`, JSON.stringify(channel))
+        return merge(channel, {id: id})
+      }catch(err){
+        return merge(null)
+      }
+
     },
     get: async (id) => {
+      let channel;
       if(!id) throw Error('Invalid id')
-      const data = await db.get(`channels:${id}`)
-      const channel = JSON.parse(data)
-      return merge(channel, {id: id})
+      await new Promise(async (resolve,reject) => {
+        await db.get(`channels:${id}`,(err,res) => {
+          try{
+            if(!res) throw Error('Not existing channel')
+            else{
+              channel = JSON.parse(res)
+              resolve(channel)
+            }
+          }catch(err){
+            reject(Error(err))
+          }
+        })
+      })
     },
     list: async (id) => {
       return new Promise( (resolve, reject) => {
@@ -37,10 +53,21 @@ module.exports = {
         })
       })
     },
-    update: (id, channel) => {
-      const original = store.channels[id]
-      if(!original) throw Error('Unregistered channel id')
-      store.channels[id] = merge(original, channel)
+    update: async (id, channel) => {
+      if(!id) throw Error('Unregistered channel id')
+      await new Promise(async (resolve,reject) => {
+        await db.get(`channels:${id}`,async (err,res) => {
+          try{
+            if(!res) throw Error("Channel doesn't exist")
+            res = JSON.parse(res)
+            await db.put(`channels:${id}`, JSON.stringify(channel))
+            resolve()
+          }catch(err){
+            reject(Error(err))
+          }
+        })
+      })
+      return merge(channel, {id: id})
     },
     delete: async (id) => {
       if(!id) throw Error('No id')
@@ -51,29 +78,42 @@ module.exports = {
           console.log(err,res)
         })
       })
-      
     },
     deleteUser: async (id, user) => {
-      await db.get(`channels:${id}`,async (err,data)=>{
-        data = JSON.parse(data)
-        const val = data.users.filter(i => i !== user)
-        await db.put(`channels:${id}`, JSON.stringify({
-          name: data.name,
-          users: val
-        }))
+      await new Promise(async (resolve,reject) => {
+        await db.get(`channels:${id}`,async (err,data)=>{
+          data = JSON.parse(data)
+          const val = data.users.filter(i => i !== user)
+          await db.put(`channels:${id}`, JSON.stringify({
+            name: data.name,
+            users: val
+          }))
+          resolve()
+        })
       })
     },
     updateUser: async (id, users) => {
-      await db.get(`channels:${id}`,async (err,data)=>{
-        data = JSON.parse(data)
-        users.users.map((user) => {
-          data.users = [...data.users,user]
+      await new Promise(async (resolve,reject) => {
+        await db.get(`channels:${id}`,async (err,res) => {
+          try{
+            if(!res) throw Error("Channel doesn't exist")
+            res = JSON.parse(res)
+            users.users.map((user) => {
+              if(res.users.includes(user))
+                throw Error("User already in")
+              res.users = [...res.users,user]
+            })
+            await db.put(`channels:${id}`, JSON.stringify({
+              name: res.name,
+              users: res.users
+            }))
+            resolve()
+          }catch(err){
+            reject(Error(err))
+          }
         })
-        await db.put(`channels:${id}`, JSON.stringify({
-          name: data.name,
-          users: data.users
-        }))
       })
+      return merge(users, {id: id})
     },
   },
   messages: {
@@ -131,7 +171,7 @@ module.exports = {
       return merge(user, {id: id})
     },
     get: async (id) => {
-      if(!id) throw Error('Invalid id')
+      if(!id) throw Error('Invalid email')
       return new Promise( (resolve, reject) => {
         let users;
         db.createReadStream({
@@ -146,10 +186,15 @@ module.exports = {
         }).on( 'error', (err) => {
           reject(err)
         }).on( 'end', () => {
-          console.log(users,"ici")
           resolve(users)
         })
       })
+    },
+    getId: async (id) => {
+      if(!id) throw Error('Invalid id')
+      const data = await db.get(`users:${id}`)
+      const user = JSON.parse(data)
+      return merge(user, {id: id})
     },
     getChannels: async (id)=> {
       const data = await db.get(`users:${id}`)
@@ -173,10 +218,26 @@ module.exports = {
         })
       })
     },
-    update: (id, user) => {
-      const original = store.users[id]
-      if(!original) throw Error('Unregistered user id')
-      store.users[id] = merge(original, user)
+    update: async (id, user) => {
+      if(!id) throw Error('Unregistered user id')
+      await new Promise(async (resolve,reject) => {
+        await db.get(`users:${id}`,(err,res) => {
+          try{
+            if(!res) throw Error("User doesn't exist")
+            res = JSON.parse(res)
+            if(res.email !== user.email){
+                throw Error('Cannot edit email')
+            }
+            else{
+              db.put(`users:${id}`, JSON.stringify(user))
+              resolve()
+            }
+          }catch(err){
+            reject(Error(err))
+          }
+        })
+      })
+      return merge(user, {id: id})
     },
     delete: (id, user) => {
       const original = store.users[id]
@@ -210,16 +271,26 @@ module.exports = {
       })
     },
     removeChannel: async (user,id) => {
-      const data = await db.get(`users:${user}`, async (err,res) => {
-        res = JSON.parse(res)
-        const index = res.channels.indexOf(id)
-        res.channels.splice(index,1)
-        await db.put(`users:${user}`, JSON.stringify({
-          email: res.email,
-          firstName: res.firstName,
-          lastName: res.lastName,
-          channels: res.channels
-        }))
+      await new Promise(async (resolve,reject) => {
+        await db.get(`users:${user}`, async (err,res) => {
+          try{
+            res = JSON.parse(res)
+            const index = res.channels.indexOf(id)
+            if(index===-1){
+              throw Error('not in this channel')
+            }
+            res.channels.splice(index,1)
+            const data = await db.put(`users:${user}`, JSON.stringify({
+              email: res.email,
+              firstName: res.firstName,
+              lastName: res.lastName,
+              channels: res.channels
+            }))
+            resolve()
+          }catch(err){
+            reject(Error(err))
+          }
+        })
       })
     }
   },
